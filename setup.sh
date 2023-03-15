0191f379cedbba5c5ec1afcb156f83ca698071f0
ncouro <nicolasc@graphcore.ai>
2023-03-15 18:05:46 +0000

    Disable datasets mounts

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ export POPTORCH_CACHE_DIR="${POPLAR_EXECUTABLE_CACHE_DIR}"
 export POPTORCH_LOG_LEVEL=ERR
 export RDMAV_FORK_SAFE=1
 
-nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
+#nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
 
 export PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp
 jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
